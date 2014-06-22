20eaec5a2a7a7ee7e14ba6fa45701ff64270bd20
eightnoteight <mr.eightnoteight@gmail.com>
2014-06-22 15:46:04 +0530

    uh oh, some bugs have crept in

--- 2048.c
+++ 2048.c
@@ -161,13 +161,14 @@ boool gameinit()
 		}
 	}
 }
-void Winner()
-{
-
+void Winner(){
+	clear();
+	mvprintw(row/2,(col-10)/2, "WINNER! :)");
 }
-void Loser()
-{
 
+void Loser(){
+	clear();
+	mvprintw(row/2,(col-8)/2, "LOSER :(");
 }
 int main(int argc, char const *argv[])
 {
